4533748db3bbd2f4251c851c71a8fc595cd55aa9
Franck024 <fcozzolino24@gmail.com>
2023-05-06 14:48:54 +0200

    creato il menu

--- Esercizio1/exercise1/main.cpp
+++ Esercizio1/exercise1/main.cpp
@@ -26,8 +26,43 @@
 
 /* ************************************************************************** */
 
+
+
 int main() {
+  int choise;
   std::cout << "Lasd Libraries 2023" << std::endl;
-  lasdtest(); // Call in the menu of your library test!
+  std::cout << std::endl;
+  std::cout << "Cozzolino Francesco  N86002831" << std::endl;
+  std::cout << std::endl;
+  std::cout << std::endl;
+
+  do{
+    std::cout << "Digitare il comando:" << std::endl;
+    std::cout << "   1. Eseguire il SimpleTest (test del professore)" << std::endl;
+    std::cout << "   2. Eseguire il test dello studente" << std::endl;
+    std::cout << "   3. Chiudere il programma" << std::endl;
+
+    std::cin >> choise;
+
+    while(choise < 1 || choise > 3){
+      std::cout << "Scelta non corretta. Riprova:" << std::endl;
+      std::cin >> choise;
+    }
+
+    if (choise == 1) {
+      std::cout << "Hai scelto l'opzione 1.\n";
+      lasdtest(); // Call in the menu of your library test!
+    if (choise == 2){
+      std::cout << "Hai scelto l'opzione 2.\n";
+    }
+          
+    if (choise == 3)
+      std::cout << "Chiusura programma \n";
+      break;
+    }
+  }while(true);
+  
   return 0;
 }
+
+
