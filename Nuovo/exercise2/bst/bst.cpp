3bbf7d05396d8d6def198608a68aac5d030c9673
Franck024 <fcozzolino24@gmail.com>
2023-08-29 21:05:31 +0200

    risolti errori bst
    ridefinite le funzioni: min/max e equivalenti, successor/predecessor e equivalenti e i vari detach

--- Nuovo/exercise2/bst/bst.cpp
+++ Nuovo/exercise2/bst/bst.cpp
@@ -6,6 +6,7 @@ namespace lasd {
   // Specific constructors
   template <typename Data>
   BST<Data>::BST(const LinearContainer<Data>& mp) {
+    size = 0;
     for(ulong i = 0; i < mp.Size(); i++){
         Insert(mp[i]);
     }
@@ -13,6 +14,8 @@ namespace lasd {
 
   template <typename Data>
   BST<Data>::BST(MutableMappableContainer<Data>&& mp)  {
+    size = 0;
+    root = mp.Root();
     mp.Map([&](Data&& item) {
         Insert(std::move(item));
     });
@@ -74,26 +77,25 @@ namespace lasd {
   template <typename Data>
   Data& BST<Data>::Min()  {
     if(root == nullptr)
-        throw std::length_error("Albero vuoto");
+        throw std::length_error("Albero vuoto ccc");
     return (FindPointerToMin(root))->it;
   } 
 
   template <typename Data>
   Data BST<Data>::MinNRemove()  {
-    if(root == nullptr)
-        throw std::length_error("Albero vuoto");
-  struct BST<Data>::NodeLnk* minNode = DetachMin(root);
-  Data minValue = minNode->it;
-  delete minNode;
-  return minValue;
+    if(root == nullptr) throw std::length_error("Albero vuoto bb");
+  Data val = Min();
+  RemoveMin();
+  return val;
   } 
   
   template <typename Data>
   void BST<Data>::RemoveMin()  {
     if(root == nullptr)
-        throw std::length_error("Albero vuoto");
-    struct BST<Data>::NodeLnk* minNode = DetachMin(root);
-    delete minNode;
+        throw std::length_error("Albero vuoto aaaaaaaa");
+    
+    DetachMin(root);
+  //  delete minNode;
   }  
 
   template <typename Data>
@@ -105,25 +107,23 @@ namespace lasd {
 
   template <typename Data>
   Data BST<Data>::MaxNRemove()  {
-    if(root == nullptr)
-        throw std::length_error("Albero vuoto");
-    struct BST<Data>::NodeLnk* maxNode = DetachMax(root);
-    Data maxValue = maxNode->it;
-    delete maxNode;
+    if(root == nullptr) throw std::length_error("Albero vuoto");
+    Data maxValue = Max();
+    RemoveMax();
     return maxValue;
+    // return DataNDelete(DetachMax(root));
   } 
 
   template <typename Data>
   void BST<Data>::RemoveMax()  {
     if(root == nullptr)
         throw std::length_error("Albero vuoto");
-    delete DetachMax(root);
+    DetachMax(root);
   } 
 
   template <typename Data>
   Data& BST<Data>::Predecessor(const Data& value)  {
-    if(root == nullptr)
-        throw std::length_error("Vuoto");
+    if(root == nullptr) throw std::length_error("Vuoto");
     struct BST<Data>::NodeLnk* preNode = FindPointerToPredecessor(value, root);
     if(preNode == nullptr) throw std::out_of_range("Nessun predecessore trovato");
     return preNode->it;
@@ -151,10 +151,10 @@ namespace lasd {
 
   template <typename Data>
   Data& BST<Data>::Successor(const Data& value)  {
-    if(root == nullptr)
-        throw std::length_error("Vuoto");
+    if(root == nullptr) throw std::length_error("Vuoto");
     struct BST<Data>::NodeLnk* succNode = FindPointerToSuccessor(value, root);
-    if(succNode == nullptr) throw std::out_of_range("Nessun predecessore trovato");
+    if(succNode == nullptr) throw std::out_of_range("Nessun successore trovato");
+    if(succNode->it <= value) throw std::out_of_range("Nessun successore trovato");
     return succNode->it;
   }  
 
@@ -171,11 +171,28 @@ namespace lasd {
 
   template <typename Data>
   void BST<Data>::RemoveSuccessor(const Data& value)  {
-    if(root == nullptr)
-        throw std::length_error("Vuoto");
+    // if(root == nullptr) throw std::length_error("Vuoto");
+    // struct BST<Data>::NodeLnk* succNode = FindPointerToSuccessor(value, root);
+    // if(succNode == nullptr) return;
+    // Detach(succNode);
+    //---------------------------------
+    if(root == nullptr) throw std::length_error("Vuoto");
+    struct BST<Data>::NodeLnk* node = FindPointerTo(value, root);
+    if (node == nullptr) throw std::invalid_argument("Nodo non trovato");
     struct BST<Data>::NodeLnk* succNode = FindPointerToSuccessor(value, root);
-    if(succNode == nullptr) return;
-    Detach(succNode);
+    if(succNode == nullptr) throw std::out_of_range("Successore non trovato");
+    if(succNode->rc != nullptr){
+      struct BST<Data>::NodeLnk* succR = succNode->rc;
+      succNode->it = succR->it;
+      succNode->lc = succR->lc;
+      succNode->rc = succR->rc;
+      delete succR;
+    }else{
+      if(succNode == node->lc) node->lc = nullptr;
+      else node->rc = nullptr;
+      delete succNode;
+    }
+    size--;
   } 
 
   /* ************************************************************************ */
@@ -183,90 +200,33 @@ namespace lasd {
   // Specific member functions (inherited from DictionaryContainer)
   template <typename Data>
   bool BST<Data>::Insert(const Data& dato)  {
-    // struct BST<Data>::NodeLnk*& pointer = FindPointerTo(dato, root);
-    // if(pointer==nullptr){
-    //     pointer = new struct BST<Data>::NodeLnk(dato);
-    //     size++;
-    //     return true;
-    // }
-    // return false;
-    //-----------------------------------
-    struct BST<Data>::NodeLnk* node = new struct BST<Data>::NodeLnk(dato);
-    if(root == nullptr){
-      root = node;
+    struct BST<Data>::NodeLnk*& node = FindPointerTo(dato, root);
+    if(node == nullptr){
+      node = new struct BST<Data>::NodeLnk(dato);
       size++;
       return true;
     }
-    struct BST<Data>::NodeLnk* curr = root;
-    struct BST<Data>::NodeLnk* parent = nullptr;
-    while(curr != nullptr){
-      parent = curr;
-      if(dato < curr->it) curr = curr->lc;
-      else if(dato > curr->it) curr = curr->rc;
-        else{
-          delete node;
-          return false;
-        }
-    }
-    if(dato < parent->it) parent->lc = node;
-    else parent->rc = node;
-    size++; 
-    return true;
+    return false;
   }  
 
   template <typename Data>
   bool BST<Data>::Insert(Data&& dato) noexcept  {
-    struct BST<Data>::NodeLnk* node = new struct BST<Data>::NodeLnk(dato);
-    if(root == nullptr){
-      root = node;
+    struct BST<Data>::NodeLnk*& node = FindPointerTo(dato, root);
+    if(node == nullptr){
+      node = new struct BST<Data>::NodeLnk(dato);
       size++;
       return true;
     }
-    struct BST<Data>::NodeLnk* curr = root;
-    struct BST<Data>::NodeLnk* parent = nullptr;
-    while(curr != nullptr){
-      parent = curr;
-      if(dato < curr->it) curr = curr->lc;
-      else if(dato > curr->it) curr = curr->rc;
-        else{
-          delete node;
-          return false;
-        }
-    }
-    if(dato < parent->it) parent->lc = node;
-    else parent->rc = node;
-    size++; 
-    return true;
+    return false;
   } 
 
   template <typename Data>
   bool BST<Data>::Remove(const Data& dato)  {
-    if(root == nullptr) std::length_error("Albero vuoto");
-    struct BST<Data>::NodeLnk* parent = nullptr;
-    struct BST<Data>::NodeLnk* delNode = FindPointerTo(dato, root);
-    if(delNode == nullptr) return false;
-    if(delNode->lc == nullptr){
-      if(parent == nullptr) root = delNode->rc;
-      else if(parent->lc == delNode) parent->lc = delNode->rc;
-        else parent->rc = delNode->rc;
-      delete delNode;
-    }else if(delNode->rc == nullptr){
-      if(parent == nullptr) root = delNode->lc;
-      else if(parent->lc == delNode) parent->lc = delNode->lc;
-        else parent->rc = delNode->lc;
-      delete delNode;
-    }else{
-      struct BST<Data>::NodeLnk* succ = delNode->rc;
-      struct BST<Data>::NodeLnk* succParent = delNode;
-      while(succ->lc != nullptr){
-        succParent = succ;
-        succ = succ->lc;
-      }
-      delNode->it = succ->it;
-      if(succParent->lc == succ) succParent->lc = succ->rc;
-      else succParent->rc = succ->rc;
-      delete succ;
-    }
+    if(root == nullptr) throw std::length_error("Albero vuoto");
+    struct BST<Data>::NodeLnk*& delNodePtr = FindPointerTo(dato, root);
+    if(delNodePtr == nullptr) return false;
+    Detach(delNodePtr);
+    //delete delNode;
     return true;
   }  
 
@@ -275,7 +235,7 @@ namespace lasd {
   // Specific member functions (inherited from TestableContainer)
   template <typename Data>
   bool BST<Data>::Exists(const Data& dato) const noexcept  {
-    return (FindPointerTo(dato, root)!=nullptr);
+    return FindPointerTo(dato, root)!=nullptr;
   } 
 
   /* ************************************************************************ */
@@ -294,6 +254,7 @@ namespace lasd {
         ClearNodes(node->lc);
         ClearNodes(node->rc); 
         delete node;
+        node = nullptr;
     }
 }
 
@@ -303,62 +264,67 @@ namespace lasd {
   Data BST<Data>::DataNDelete(struct BST<Data>::NodeLnk* node)  {
     if(node == nullptr) throw std::invalid_argument("Nodo non trovato");
     Data val = node->it;
-    Remove(node->it);
+    Detach(node);
     return val;
   } 
 
   template <typename Data>
   struct BST<Data>::NodeLnk* BST<Data>::Detach(struct BST<Data>::NodeLnk*& node) noexcept {
     if(node == nullptr) return nullptr;
-    struct BST<Data>::NodeLnk* detNode = nullptr;
-    if(node->lc == nullptr) detNode =  Skip2Right(node);
-    else if(node->rc == nullptr) detNode = Skip2Left(node);
-    else{
-        detNode = DetachMax(node->lc);
-        detNode->lc = node->lc;
-        detNode->rc = node->rc;
-    }
-    delete node;
-    node = detNode;
-    size--;
-    return detNode;
+
+    if(node->lc == nullptr) return Skip2Right(node);
+    else if(node->rc == nullptr) return Skip2Left(node);
+      else{
+        struct BST<Data>::NodeLnk* detach = DetachMax(node->lc);
+        node->it = detach->it;
+        return detach;
+      }return nullptr;
   } 
 
   template <typename Data>
   struct BST<Data>::NodeLnk* BST<Data>::DetachMin(struct BST<Data>::NodeLnk*& node) noexcept {
-    struct BST<Data>::NodeLnk* minNode = FindPointerToMin(node);
-    if(minNode->lc == nullptr) node = minNode->rc;
-    else minNode->lc = Skip2Right(minNode->lc);
-    size--;
-    return minNode;
+    if(node == nullptr) return nullptr;
+    if(node->lc == nullptr){
+      struct BST<Data>::NodeLnk* minNode = node;
+      node = node->rc;
+      size--;
+      return minNode;
+    }
+    return DetachMin(node->lc);
   } 
 
   template <typename Data>
   struct BST<Data>::NodeLnk* BST<Data>::DetachMax(struct BST<Data>::NodeLnk*& node) noexcept {
-    struct BST<Data>::NodeLnk* maxNode = FindPointerToMax(node);
-    if(maxNode->rc == nullptr) node = maxNode->lc;
-    else maxNode->rc = Skip2Left(maxNode->rc);
-    size--;
-    return maxNode;
+    if(node == nullptr) return nullptr;
+    // struct BST<Data>::NodeLnk* maxNode = FindPointerToMax(node);
+    // if(maxNode->rc == nullptr) root = maxNode->lc;
+    // else maxNode->rc = Skip2Left(maxNode->rc);
+    // size--;
+    // return maxNode;
+    if(node->rc == nullptr){
+      struct BST<Data>::NodeLnk* maxNode = node;
+      node = node->lc;
+      size--;
+      return maxNode;
+    }
+    return DetachMax(node->rc);
   } 
 
   template <typename Data>
   struct BST<Data>::NodeLnk* BST<Data>::Skip2Left(struct BST<Data>::NodeLnk*& node) noexcept {
-    struct BST<Data>::NodeLnk* skipLeft = nullptr;
-    if(node != nullptr){
-        skipLeft = node->lc;
-        delete node;
-    }
+    if(node == nullptr) return nullptr;
+    struct BST<Data>::NodeLnk* skipLeft = node->lc;
+    delete node;   
+    node = nullptr; 
     return skipLeft;
   } 
 
   template <typename Data>
-  struct BST<Data>::NodeLnk* BST<Data>::Skip2Right(struct BST<Data>::NodeLnk*& node) noexcept {
-    struct BST<Data>::NodeLnk* skipRight = nullptr;
-    if(node != nullptr){
-        skipRight = node->rc;
-        delete node;
-    }
+  struct BST<Data>::NodeLnk* BST<Data>::Skip2Right(struct BST<Data>::NodeLnk*& node) noexcept {  
+    if(node == nullptr) return nullptr;
+    struct BST<Data>::NodeLnk* skipRight = node->rc;
+    delete node;
+    node = nullptr;
     return skipRight;
   } 
 
@@ -400,50 +366,6 @@ namespace lasd {
 
   template <typename Data>
   struct BST<Data>::NodeLnk* const& BST<Data>::FindPointerTo(const Data& dato, struct BST<Data>::NodeLnk* const& node) const noexcept {
-    // std::cout << std::endl << "------HERE   F-------------" << std::endl;
-    // struct BST<Data>::NodeLnk* const* ptr = &node;
-    // std::cout << std::endl << "------HERE  F 1 -------------" << std::endl;
-    // if(*ptr != nullptr){
-    // std::cout << std::endl << "------HERE   F 2-------------" << std::endl;
-    //     if(node->it == dato){
-    // std::cout << std::endl << "------HERE  Fc3  -------------" << std::endl;
-    //       return *ptr;
-    //     } 
-    //     else if(node->it > dato){
-    // std::cout << std::endl << "------HERE   F 4-------------" << std::endl;
-    //       ptr = &((*this).FindPointerTo(dato, node->lc));
-    //     } 
-    //     else {
-    // std::cout << std::endl << "------HERE  F5 -------------" << std::endl;
-    //       ptr = &((*this).FindPointerTo(dato, node->rc));
-    // }}
-    // std::cout << std::endl << "------HERE  F EXT -------------" << std::endl;
-    // return *ptr;
-    //-----------------------------------------------------------------------
-    // std::cout << std::endl << "------HERE   4-------------" << std::endl;
-    //     struct BST<Data>::NodeLnk* const* pointer = &node;
-    // struct BST<Data>::NodeLnk* current = node;
-
-    // if(current!=nullptr) {
-    // std::cout << std::endl << "------HERE   5-------------" << std::endl;
-    //     while (current!=nullptr && current->it!=dato) {
-    // std::cout << std::endl << "------HERE   while-------------" << std::endl;
-    //         if(dato < current->it  ) {
-    // std::cout << std::endl << "------curr->lc=    "<< current->lc << std::endl;
-    //             pointer = &current->lc;
-    // std::cout << std::endl << "------HERE   xxx-------------" << std::endl;
-    //             current = current->lc;
-    // std::cout << std::endl << "------HERE  ssss -------------" << std::endl;
-    //         }else if(dato > current->it  ){
-    // std::cout << std::endl << "------HERE  w elseif -------------" << std::endl;
-    //             pointer = &current->rc;
-    //             current = current->rc;
-    //         }
-    //     }
-    // }
-    // std::cout << std::endl << "------FINE   -------------" << std::endl;
-    // return *pointer;  
-    //-----------------------------------------------------------------------
       if(node == nullptr || node->it == dato) return node;
       if(node->it > dato) return FindPointerTo(dato, node->lc);
       if(node->it < dato) return FindPointerTo(dato, node->rc);
@@ -452,27 +374,14 @@ namespace lasd {
 
   template <typename Data>
   struct BST<Data>::NodeLnk*& BST<Data>::FindPointerTo(const Data& dato, struct BST<Data>::NodeLnk*& node) noexcept {
-          if(node == nullptr || node->it == dato) return node;
+      if(node == nullptr || node->it == dato) return node;
       if(node->it > dato) return FindPointerTo(dato, node->lc);
       if(node->it < dato) return FindPointerTo(dato, node->rc);
       return node;
-
   }  
  
   template <typename Data>
   struct BST<Data>::NodeLnk* const& BST<Data>::FindPointerToPredecessor(const Data& dato, struct BST<Data>::NodeLnk* const& node) const noexcept {
-    // struct BST<Data>::NodeLnk* const* current = &node;
-    // struct BST<Data>::NodeLnk* const* estimate = nullptr;
-    // while(*current != nullptr && (*current)->it != dato){
-    //     if((*current)->it < dato) current = &((*current)->rc);
-    //     else{
-    //         estimate = current;
-    //         current = &((*current)-> rc);
-    //     }
-    // }
-    // if(*current != nullptr && (*current)->HasLeftChild()) estimate = &(FindPointerToMax((*current)->lc));
-    // return *estimate;
-
     struct BST<Data>::NodeLnk* pre = nullptr;
     while(node != nullptr){
         if(node->it <dato){
@@ -485,18 +394,6 @@ namespace lasd {
 
   template <typename Data>
   struct BST<Data>::NodeLnk* BST<Data>::FindPointerToPredecessor(const Data& dato, struct BST<Data>::NodeLnk*& node) noexcept {
-    // struct BST<Data>::NodeLnk* * current = &node;
-    // struct BST<Data>::NodeLnk* * estimate = nullptr;
-    // while(*current != nullptr && (*current)->it != dato){
-    //     if((*current)->it < dato) current = &((*current)->rc);
-    //     else{
-    //         estimate = current;
-    //         current = &((*current)-> rc);
-    //     }
-    // }
-    // if(*current != nullptr && (*current)->HasLeftChild()) estimate = &(FindPointerToMax((*current)->lc));
-    // return *estimate;
-    
     struct BST<Data>::NodeLnk* pre = nullptr;
     while(node != nullptr){
         if(node->it < dato){
